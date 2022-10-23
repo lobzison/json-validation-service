763ab5bb363c40e4a3d5087777e21c14e4b25ca2
Gleb Lobov <lobzison@gmail.com>
2022-10-23 15:48:22 +0200

    Tidy up Main

--- src/main/scala/validator/Main.scala
+++ src/main/scala/validator/Main.scala
@@ -1,30 +1,34 @@
 package validator
 
 import cats.effect.{IO, IOApp}
+import doobie.Transactor
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
-import validator.api.RoutesImpl
-import validator.persistence.SchemaPersistenceImpl
+import validator.api.{Routes, RoutesImpl}
+import validator.persistence._
 import validator.persistence.db.DBImpl
-import validator.service.{JsonParserServiceImpl, SchemaValidationServiceImpl, ServiceImpl}
+import validator.service._
 
 object Main extends IOApp.Simple {
   override def run: IO[Unit] = {
-    val db = new DBImpl[IO]
-    db.transactor.use { xa =>
-      val persistence = new SchemaPersistenceImpl[IO](xa)
-      val validation  = new SchemaValidationServiceImpl[IO]
-      val parsing     = new JsonParserServiceImpl[IO]
-
-      val service = new ServiceImpl[IO](persistence, parsing, validation)
-
-      val routes = new RoutesImpl[IO](service)
-
-      EmberServerBuilder
-        .default[IO]
-        .withHttpApp(Router("/" -> routes.routes).orNotFound)
-        .build
-        .useForever
+    new DBImpl[IO].transactor.use { xa =>
+      program(xa)
     }
+  }
+
+  def program(xa: Transactor[IO]): IO[Unit] = {
+    val persistence: SchemaPersistence[IO]      = new SchemaPersistenceImpl[IO](xa)
+    val validation: SchemaValidationService[IO] = new SchemaValidationServiceImpl[IO]
+    val parsing: JsonParserService[IO]          = new JsonParserServiceImpl[IO]
+
+    val service: Service[IO] = new ServiceImpl[IO](persistence, parsing, validation)
+
+    val routes: Routes[IO] = new RoutesImpl[IO](service)
+
+    EmberServerBuilder
+      .default[IO]
+      .withHttpApp(Router("/" -> routes.routes).orNotFound)
+      .build
+      .useForever
   }
 }
